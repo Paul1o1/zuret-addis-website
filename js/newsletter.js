b8084fe4fd9e403d35b01d2013c3d80b977f71db
paul1o1 <pablonigussie1@gmail.com>
2023-09-27 15:08:01 +0100

    initial commit

---
+++ js/newsletter.js
@@ -0,0 +1,10 @@
+
+                        function ml_webform_success_7730397() {
+                          var $ = ml_jQuery || jQuery;
+                          $('.ml-subscribe-form-7730397 .row-success').show();
+                          $('.ml-subscribe-form-7730397 .row-form').hide();
+                        }
+                          
+                      
+                      
+                         
